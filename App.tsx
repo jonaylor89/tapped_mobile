5e7b084f11c7c1feb582775e669ef3d3cc772dcc
Johannes Naylor <jonaylor89@gmail.com>
2022-04-30 14:49:50 -0400

    add react-navigation

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Tapped is the best</Text>
     </View>
   );
 }
