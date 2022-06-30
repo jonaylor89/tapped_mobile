c2b0df1991ce8caf6c127ab11745dc9d2ecbe9a8
batmaninrobin <malikmui@vcu.edu>
2022-06-30 10:59:44 -0400

    working on layout

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,13 +1,38 @@
 
-import { StyleSheet,Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button
+} from 'react-native';
+import { SocialIcon } from 'react-native-elements';
 
 
 const ProfileScreen = () => {
     return (
         <View style={styles.container} >
-            <Text>Profile Screen</Text>
-            <Text>Profile Page - Test</Text>
-            <Text>Profile Page - Michael</Text>
+          <View>
+            <Image>
+              {/* <source /> = {require('~/Repos/CMSC630_Image_Analysis/datasets/Cancerous_cell_smears/cyl11.BMP')} */}
+            </Image>
+            <Text>
+              Kid Kahuna -- make this changeable via edit Profile
+            </Text>
+          </View>
+          <View>
+            <Button title='Edit' style={{
+              height: 60,
+              width: 60,
+              backgroundColor: "white"
+            }}
+            />
+            <SocialIcon button type='twitter'/>
+            <SocialIcon button type='tiktok'/>
+            <SocialIcon button type='spotify'/>
+            <SocialIcon button type='soundcloud'/>
+            <SocialIcon button type='instagram'/>
+            <SocialIcon button type='youtube'/>
+          </View>
+          <View>
+            <Text>More Info</Text>
+            <Text>Lorem Ipsum Dolor Sit</Text>
+          </View>
         </View>
     );
 }
