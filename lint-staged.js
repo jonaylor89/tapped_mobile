ac10282b30258a9415cb00dbf0a229c5300aff85
Johannes Naylor <jonaylor89@gmail.com>
2022-09-05 11:15:07 -0400

    linting

--- lint-staged.js
+++ lint-staged.js
@@ -1,7 +1,7 @@
 module.exports = {
     '*.{js,jsx,ts,tsx}': [
         'eslint --ext .ts,.tsx src',
-        'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
+        // 'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
         () => 'tsc-files --noEmit',
     ],
     '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
